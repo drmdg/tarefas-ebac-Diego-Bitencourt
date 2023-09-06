72dff2bd00e684720c9701c5994b9ada6b0d9783
drmdg <diegobitencort@gmail.com>
2023-09-06 15:57:07 -0300

    done

---
+++ mod8/TarefaCalculoMedia/src/CalculadoraMedia.java
@@ -0,0 +1,6 @@
+public class CalculadoraMedia {
+    public void calcularMedia(double n1,double n2,double n3,double n4){
+        double media = (n1+n2+n3+n4)/4;
+        System.out.println(media);
+    }
+}
---
+++ mod8/TarefaCalculoMedia/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        CalculadoraMedia calculadoraMedia = new CalculadoraMedia();
+
+        calculadoraMedia.calcularMedia(8.0,12.0,15.0,5.0);
+    }
+}
