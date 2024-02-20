60f4dde929e1d00062f4719a4900876c9aa0e4e5
drmdg <diegobitencort@gmail.com>
2024-02-20 13:24:30 -0300

    done

---
+++ mod0-10/mod0-10/toggle/toggle-server/src/test/java/br/com/ebac/toggleserver/ToggleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ebac.toggleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToggleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
