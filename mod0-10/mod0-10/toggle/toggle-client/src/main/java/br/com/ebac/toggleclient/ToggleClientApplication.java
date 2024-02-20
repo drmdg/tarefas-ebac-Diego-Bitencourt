fcd4af4f77107ca0270186e85c79f4c624249093
drmdg <diegobitencort@gmail.com>
2024-02-20 13:27:29 -0300

    done

---
+++ mod0-10/mod0-10/toggle/toggle-client/src/main/java/br/com/ebac/toggleclient/ToggleClientApplication.java
@@ -0,0 +1,13 @@
+package br.com.ebac.toggleclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToggleClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToggleClientApplication.class, args);
+	}
+
+}
