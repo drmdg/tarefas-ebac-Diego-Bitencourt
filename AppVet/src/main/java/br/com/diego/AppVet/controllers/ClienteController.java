fa13169901d8734c644850cdf253a1d50eb21405
drmdg <diegobitencort@gmail.com>
2024-03-27 14:14:11 -0300

    done

--- AppVet/src/main/java/br/com/diego/AppVet/controllers/ClienteController.java
+++ AppVet/src/main/java/br/com/diego/AppVet/controllers/ClienteController.java
@@ -5,6 +5,7 @@ import br.com.diego.AppVet.dtos.EnderecoDto;
 import br.com.diego.AppVet.models.Cliente;
 import br.com.diego.AppVet.models.Endereco;
 import br.com.diego.AppVet.services.ClienteService;
+import br.com.diego.AppVet.services.EnderecoService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,12 +21,39 @@ public class ClienteController {
 
     @Autowired
     ClienteService clienteService;
+    @Autowired
+    EnderecoService enderecoService;
+
+
+
+
     @PostMapping
-    public ResponseEntity<Object> saveCliente(@RequestBody ClienteDto clienteDto){
+    public ResponseEntity<Object> saveCliente(@RequestBody ClienteDto clienteDto) {
+        // Cria um novo cliente
         Cliente cliente = new Cliente();
-        BeanUtils.copyProperties(clienteDto,cliente);
-        return ResponseEntity.status(HttpStatus.CREATED).body(clienteService.save(cliente));
 
+        // Copia as propriedades do clienteDto para o cliente
+        BeanUtils.copyProperties(clienteDto, cliente);
+
+        // Verifica se o endereço do cliente está presente no DTO
+        if (clienteDto.endereco() != null) {
+            // Obtém o endereco do DTO
+            Endereco endereco = clienteDto.endereco();
+
+            // Verifica se o endereco já existe no banco de dados
+            if (endereco.getId() != null) {
+                // Mescla o endereco com o estado persistido
+                endereco = enderecoService.merge(endereco);
+            }
+
+            // Associa o endereco ao cliente
+            cliente.setEndereco(endereco);
+        }
+
+        // Salva o cliente com o endereco associado
+        Cliente savedCliente = (Cliente) clienteService.save(cliente);
+
+        // Retorna a resposta com o cliente salvo
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCliente);
     }
-
 }
--- AppVet/src/main/java/br/com/diego/AppVet/services/EnderecoService.java
+++ AppVet/src/main/java/br/com/diego/AppVet/services/EnderecoService.java
@@ -2,8 +2,13 @@ package br.com.diego.AppVet.services;
 
 import br.com.diego.AppVet.models.Endereco;
 import br.com.diego.AppVet.repositories.EnderecoRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EnderecoService {
@@ -11,7 +16,20 @@ public class EnderecoService {
     @Autowired
     EnderecoRepository enderecoRepository;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
     public Object save(Endereco endereco) {
         return enderecoRepository.save(endereco);
+    }
+
+    public List<Endereco> findAll() {
+        return enderecoRepository.findAll();
+    }
+
+    @Transactional
+    public Endereco merge(Endereco endereco) {
+        return entityManager.merge(endereco);
     }
 }
